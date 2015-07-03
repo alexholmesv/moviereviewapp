2a6bfacee09d4d0b4a28ef4c2005488223484387
Alexander Holmes <alexander.holmes@gmail.com>
2015-07-03 16:36:50 -0300

    Change model relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703151853) do
+ActiveRecord::Schema.define(version: 20150703191700) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150703151853) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "photo"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
