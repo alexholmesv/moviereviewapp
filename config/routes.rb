9956d892166b57c208a0cbd1478c1bb53f92f467
Alexander Holmes <alexander.holmes@gmail.com>
2015-07-07 15:20:09 -0300

    Add searchkick

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'landings/index'
 
   resources :movies do
+    collection do
+      get 'search'
+    end
     resources :reviews, except: [:show, :index]
   end
 
